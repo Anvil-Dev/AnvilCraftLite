48dd282df85389283ef100f5681a8ff490a500f9
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-09-08 07:09:37 +0800

    添加蟹钳和获取方式

---
+++ src/main/java/dev/anvilcraft/lite/event/LootTableEventListener.java
@@ -0,0 +1,31 @@
+package dev.anvilcraft.lite.event;
+
+import dev.anvilcraft.lite.init.item.ModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.LootTableLoadEvent;
+import org.jetbrains.annotations.NotNull;
+
+@EventBusSubscriber
+public class LootTableEventListener {
+    @SubscribeEvent
+    public static void lootTable(@NotNull LootTableLoadEvent event) {
+        ResourceLocation id = event.getName();
+        LootTable table = event.getTable();
+        if (BuiltInLootTables.FISHING_JUNK.location().equals(id)) {
+            table.addPool(
+                LootPool.lootPool()
+                    .add(LootItem.lootTableItem(ModItems.CRAB_CLAW))
+                    .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))
+                    .build()
+            );
+        }
+    }
+}
---
+++ src/main/java/dev/anvilcraft/lite/item/better/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package dev.anvilcraft.lite.item.better;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
